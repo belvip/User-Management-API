84155c81f33b2059fd157212388ca8ecee7bbaf7
belviPouadjeu <pbelvinard21@gmail.com>
2025-03-27 18:36:28 +0100

    Configuring Multiple Users InMemory  From One to Multiple Users

--- src/main/java/com/belvinard/userManagement/security/SecurityConfig.java
+++ src/main/java/com/belvinard/userManagement/security/SecurityConfig.java
@@ -4,30 +4,45 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    // /public/login
-    // /public/signup
-
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) ->
-                requests
-                        .requestMatchers("/contact").permitAll()
-                        .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/admin").denyAll()
-                        .requestMatchers("/admin/**").denyAll()
-                        .anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
         //http.formLogin(withDefaults());
-        http.sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager =
+                new InMemoryUserDetailsManager();
+        if (!manager.userExists("user1")) {
+            manager.createUser(
+                    User.withUsername("user1")
+                            .password("{noop}password1")
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if (!manager.userExists("admin")) {
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password("{noop}adminPass")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
     }
 }
