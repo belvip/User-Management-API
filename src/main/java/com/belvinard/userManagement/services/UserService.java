78f9585e45ee79c2d242ace6b784b3ba515ce54f
belviPouadjeu <pbelvinard21@gmail.com>
2025-03-28 12:27:31 +0100

    Implementation admin controller

---
+++ src/main/java/com/belvinard/userManagement/services/UserService.java
@@ -0,0 +1,14 @@
+package com.belvinard.userManagement.services;
+
+import com.belvinard.userManagement.dtos.UserDTO;
+import com.belvinard.userManagement.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void updateUserRole(Long userId, String roleName);
+
+    List<User> getAllUsers();
+
+    UserDTO getUserById(Long id);
+}
